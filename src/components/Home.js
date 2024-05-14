b33a745f7718441b05acc5d30c56d35159e2e8af
unknown <rnbrosca2003@gmail.com>
2024-05-14 17:12:31 +0300

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
           <br />
           <big>Started</big> my journey in the world of web-development with the basics of HTML and CSS where i had a lot of fun seeing how i can create something from scratch and then dove a little deeper and saw how that thing I made was able to do something with the use of JavaScript, those things made me more curios and determined me to purse the path of web development.
           </div>
-          <a href="CV Rosca Nicolae-Bogdan.pdf" target='_blank'>
+          <a href="CV Rosca Nicoale-Bogdan.pdf" target='_blank'>
           <button class="animated-button">
             <svg viewBox="0 0 24 24" class="arr-2" xmlns="http://www.w3.org/2000/svg">
               <path
